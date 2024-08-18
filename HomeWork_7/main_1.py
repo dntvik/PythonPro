df0297d3103585fc6bf9370f4123185bbbae5cb7
dntvik <viktor.ganshin@gmail.com>
2024-08-19 00:48:14 +0300

    Homework_7 (#8)
    * Homework_7
    
    * Homework_7
    
    * Homework_7 reworking

---
+++ HomeWork_7/main_1.py
@@ -0,0 +1,34 @@
+class Frange:
+    def __init__(self, start, stop=None, step:[int, float]=1):
+        if stop is None:
+            self.start = 0
+            self.stop = start
+        else:
+            self.start = start
+            self.stop = stop
+        self.step = step
+        self.current = self.start
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if (self.step > 0 and self.current >= self.stop) or (self.step < 0 and self.current <= self.stop):
+            raise StopIteration
+        current_value = round(self.current, 10)
+        self.current += self.step
+        return current_value
+
+assert list(Frange(5)) == [0, 1, 2, 3, 4]
+assert list(Frange(2, 5)) == [2, 3, 4]
+assert list(Frange(2, 10, 2)) == [2, 4, 6, 8]
+assert list(Frange(10, 2, -2)) == [10, 8, 6, 4]
+assert list(Frange(2, 5.5, 1.5)) == [2, 3.5, 5]
+assert list(Frange(1, 5)) == [1, 2, 3, 4]
+assert list(Frange(0, 5)) == [0, 1, 2, 3, 4]
+assert list(Frange(0, 0)) == []
+assert list(Frange(100, 0)) == []
+
+print('SUCCESS!')
+for i in Frange(1, 100, 3.5):
+    print(i)
