5843ae182a277a87446961187a183d87d2cc3d16
Viktor <viktor.ganshin@gmail.com>
2024-08-18 17:42:26 +0300

    Homework_6

---
+++ HomeWork_6/main.py
@@ -0,0 +1,70 @@
+import math
+
+class Shape:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def square(self):
+        return 0
+
+
+class Circle(Shape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y)
+        self.radius = radius
+
+    def square(self):
+        return math.pi*self.radius**2
+
+
+class Rectangle(Shape):
+    def __init__(self, x, y, height, width):
+        super().__init__(x, y)
+        self.height = height
+        self.width = width
+
+    def square(self):
+        return self.width*self.height
+
+
+class Parallelogram(Rectangle):
+    def __init__(self, x, y, height, width, angle):
+        super().__init__(x, y, height, width)
+        self.angle = angle
+
+    def print_angle(self):
+        print(self.angle)
+    def square(self):
+        angle_radians = math.radians(self.angle)
+        return self.width * self.height * math.sin(angle_radians)
+
+
+    def __str__(self):
+        result = super().__str__()
+        return result + f'\nParallelogram: {self.width}, {self.height}, {self.angle}'
+
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
+
+class Triangle(Shape):
+    def __init__(self, x, y, base, height):
+        super().__init__(x, y)
+        self.base = base
+        self.height = height
+
+    def square(self):
+        return 0.5 * self.base * self.height
+
+class Scene:
+    def __init__(self):
+        self._figures = []
+
+    def add_figure(self, figure):
+        self._figures.append(figure)
+
+    def total_square(self):
+        return sum(f.square() for f in self._figures)
+
+    def __str__(self):
+        pass
